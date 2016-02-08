db6c41f614c26292ce72c668aa8448556efc6cb3
John <John@10.30.19.191>
2016-02-08 15:31:19 -0600

    Did things to css

--- src/java/model/AuthorService.java
+++ src/java/model/AuthorService.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class AuthorService {
     Author first = new Author(001,"James Brown",new Date(62,4,5));
     Author second = new Author(002,"Brett Favre",new Date(89,9,3));
-    Author third = new Author(003,"Eric Dickerson",new Date(91,4,2));
+    Author third = new Author(003,"Eric Dickerson",new Date(-91,4,2));
     
     List <Author> authors = new ArrayList();
     
